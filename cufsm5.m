a9294ca4f630c93d0d261e3a4995e3fd0c020cc4
coldformed <34896711+coldformed@users.noreply.github.com>
2024-05-27 18:34:47 -0400

    Update cufsm5.m

--- cufsm5.m
+++ cufsm5.m
@@ -49,7 +49,7 @@ end
 %%-----------------------------------------------------------------------------------
 %Title and menus
 %-----------------------------------------------------------------------------------
-version=['5.30'];
+version=['5.50'];
 name=['CUFSM v',version,' -- Constrained and Unconstrained Finite Strip Method (CUFSM) Buckling Analysis of Thin-Walled Members'];
 fig=figure('Name',name,...
    	'NumberTitle','off');
